dd8193990b5013b45cc3314c0fa4deb3722882d8
rg3915 <rg3915@yahoo.com.br>
2017-01-30 17:33:11 -0200

    def person_data

--- basic/myproject/fixtures/shell_person.py
+++ basic/myproject/fixtures/shell_person.py
@@ -7,52 +7,64 @@ from myproject.selenium.gen_random_values import gen_digits, gen_cpf, gen_timest
 
 
 PHONE_TYPE = ('pri', 'com', 'res', 'cel')
+person_list = []
+phone_list = []
 REPEAT = 20
 
 
-for i in range(REPEAT):
-    g = random.choice(['M', 'F'])
-    if g == 'M':
+def person_data():
+    gender = random.choice(['M', 'F'])
+    if gender == 'M':
         treatment = gen_male_first_name()['treatment']
         first_name = gen_male_first_name()['first_name']
     else:
         treatment = gen_female_first_name()['treatment']
         first_name = gen_female_first_name()['first_name']
     last_name = names.get_last_name()
-    email = '{}.{}@example.com'.format(
-        first_name[0].lower(), last_name.lower())
-    slug = '{}-{}'.format(first_name[0].lower(), last_name.lower())
+    slug = '{}-{}'.format(first_name.lower(), last_name.lower())
+    email = slug + '@example.com'
     birthday = gen_timestamp() + '+00'
     blocked = random.choice([1, 0])
     cep = '{}-{}'.format(gen_digits(5), gen_digits(3))
     complement_ = '{} {}'.format(complement(), gen_digits(2))
-    Person.objects.create(
-        gender=g,
-        treatment=treatment,
-        first_name=first_name,
-        last_name=last_name,
-        slug=slug,
-        cpf=gen_cpf(),
-        birthday=birthday,
-        email=email,
-        address=address(),
-        complement=complement_,
-        district=district(),
-        city=city(),
-        uf=state_uf(),
-        cep=cep,
-        blocked=blocked,
-    )
+    return {'gender': gender,
+            'treatment': treatment,
+            'first_name': first_name,
+            'last_name': last_name,
+            'slug': slug,
+            'cpf': gen_cpf(),
+            'birthday': birthday,
+            'email': email,
+            'address': address(),
+            'complement': complement_,
+            'district': district(),
+            'city': city(),
+            'uf': state_uf(),
+            'cep': cep,
+            'blocked': blocked,
+            }
 
 
-''' Insert Phones '''
+# Appending person_list
+for _ in range(REPEAT):
+    person_list.append(person_data())
+
+# Insert Persons
+obj = [Person(**person) for person in person_list]
+Person.objects.bulk_create(obj)
+
+# Appending phone_list
 persons = Person.objects.all()
 for person in persons:
-    for i in range(1, random.randint(1, 5)):
-        Phone.objects.create(
-            person=person,
-            phone=gen_phone(),
-            phone_type=random.choice(PHONE_TYPE))
+    for _ in range(1, random.randint(2, 5)):
+        phone_list.append({
+            'person': person,
+            'phone': gen_phone(),
+            'phone_type': random.choice(PHONE_TYPE),
+        })
 
+# Insert Phones
+obj = [Phone(**phone) for phone in phone_list]
+Phone.objects.bulk_create(obj)
 
 print('\n%d Persons salvo com sucesso.' % REPEAT)
